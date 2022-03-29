456f6079be94b08922abea547199ac05d8273d47
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-29 10:10:10 +0700

    Update

--- ori-react-project/src/App.js
+++ ori-react-project/src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Pages/Home';
-import About_app from './Pages/About_app'
-import About_author from './Pages/About_author'
-import { BrowserRouter as Deklarasi, Routes, Route } from 'react-router-dom';
+
+import Home from "./EventHandling/Home";
 
 function App() {
   return (
-    <Deklarasi>
-      <Routes>
-        {/* <Route path="/" element={</>}/> */}
-        <Route index element={<Home />} />
-        <Route path="About_app" element={<About_app />} />
-        <Route path="About_author" element={<About_author />} />
-        {/* <Route path="*" element={<NoPage />} /> */}
-      </Routes>
-    </Deklarasi>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ ori-react-project/src/EventHandling/ListPekerjaan.jsx
@@ -0,0 +1,31 @@
+import "../App.css";
+
+const ListPekerjaan = (props) => {
+  const { data, hapusPekerjaan } = props;
+  return (
+    <div className=".App">
+      <ul style={{ listStyle: "none" }}>
+        {data.map((pekerjaan) => (
+          <li
+            style={{
+              border: "1px solid turquoise",
+              padding: "10px",
+              margin: "10px",
+            }}
+          >
+            {pekerjaan.title}
+            <button
+              onClick={() => {
+                hapusPekerjaan(pekerjaan.id);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListPekerjaan;
